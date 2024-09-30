f9fd7cd6c062af5fb1e832402f93ff7f760c8ae9
chansol4517 <ckssol4517@gmail.com>
2024-09-30 13:25:17 +0900

    스크롤시 버튼 활성화 처리

--- js/main.js
+++ js/main.js
@@ -1,33 +1,31 @@
 import Anime from "./anime.js";
 
-//이동할 세로 영역의 위치값이 담길 빈 배열 생성
+//variables
 let posArr = [];
-//main안쪽의 모든 직계자식요소들을 유사배열로 담음
 const secArr = document.querySelector("main").children;
 const scroll_btns = document.querySelectorAll(".scroller li");
 
-//세로 이동 위치값을 구해야되는 배열요소들을 반복돌며 빈배열에 이동할 위치값 등록
+//init (get section position array)
 for (let sec of secArr) posArr.push(sec.offsetTop);
 
-//scroller click event bind
+//scroll btn evnet
 scroll_btns.forEach((btn, idx) => {
-	//각 버튼 클릭시
 	btn.addEventListener("click", () => {
-		//Anime로 클릭한 버튼 순번으로 posArr에서 이동할 위치값을 적용
 		new Anime(window, { scroll: posArr[idx] });
-
-		scroll_btns.forEach(el => el.classList.remove("on"));
-		scroll_btns[idx].classList.add("on");
 	});
 });
 
-//window scroll event bind
+//window scroll event
 window.addEventListener("scroll", () => {
-	//console.log("scrollY", window.scrollY);
-	//console.log("offsetTop", banner.offsetTop);
+	const scroll = window.scrollY;
+	if (scroll >= posArr[0]) activation(scroll_btns, 0);
+	if (scroll >= posArr[1]) activation(scroll_btns, 1);
+	if (scroll >= posArr[2]) activation(scroll_btns, 2);
+	if (scroll >= posArr[3]) activation(scroll_btns, 3);
 });
 
-//미션
-//각 버튼 클릭시 해당 버튼의 활성화 처리 (on클래스 추가)
-//위와 같이 클릭으로 버튼활성화 기능을 구현했을때 발생하는 문제점 찾아보기
-//위의 문제점 파악했다면 해결 방법도 고민
+//activation func
+function activation(arrEl, index) {
+	arrEl.forEach(el => el.classList.remove("on"));
+	arrEl[index].classList.add("on");
+}
